74697e9ba7667dabea24288520499e91d0658ad8
Jay Kumar <jakumar@blackberry.com>
2015-05-14 12:04:46 -0400

    Working no cache

--- js/main.js
+++ js/main.js
@@ -1,28 +1,24 @@
 
 // ========== jQuery on click declarations
+var boxes = 0;
 $(function(){
-  $('.grab').click(function(){pasteSelection();});
+  $(document).on('click','.grab', function(){pasteSelection(this.id)});
 });
 
 $(function(){
   $('#addnew').click(function(){addnew();});
 });
 
-$(document).ready(function(){
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    document.getElementById('title').innerHTML = tabs[0].title;
-  });
-});
-
 // ========= Functions
 
 // Get selected text and paste
-function pasteSelection() {
+function pasteSelection(bid) {
+  putTitle(bid);
+  var text = document.getElementById("data".concat(bid.toString()));
   chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
   function(tab) {
     chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, 
     function(response){
-      var text = document.getElementById('data');
       if (response.data == null){
         chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
           text.innerHTML = tabs[0].url;
@@ -32,23 +28,32 @@ function pasteSelection() {
       }
     });
   });
-  addnew();
+  // addnew();
+}
+
+function putTitle(bid){
+  var p1 = document.getElementById("title".concat(bid.toString()));
+  console.log("title".concat(bid.toString()));
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    p1.innerHTML = "<b>" + tabs[0].title + "</b>";
+  });
 }
 
 // Add new paste box
-function addnew(){ 
-  document.getElementById("paste").setAttribute("class", "list-group-item grab data");
-
+function addnew(){
+  boxes+=1; 
+  // document.getElementById("paste").setAttribute("class", "list-group-item grab data");
   var button = document.createElement("BUTTON");
-  button.setAttribute("class","list-group-item active grab data")
+  button.setAttribute("class","list-group-item grab")
+  button.id = boxes.toString();
   var p = document.createElement("p");
   p.setAttribute("class", "list-group-item-heading pull-left data");
-  p.innerHTML = "Website Title"
+  p.innerHTML = "<b>Click To Paste</b>"
+  p.id = "title".concat(boxes.toString());
   var p2 = document.createElement("p");
   p2.setAttribute("class", "list-group-item-text pull-left data");
-  p2.innerHTML = "Paste Data"
+  p2.id = "data".concat(boxes.toString());
   button.appendChild(p);
   button.appendChild(p2);
   document.getElementById("snippits").appendChild(button);
-  // document.getElementById("snippits").appendChild(button);
 }
