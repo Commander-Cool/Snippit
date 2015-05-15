ed6db1df12c91045bd161865ed8d87c79a4738d5
Jay Kumar <jakumar@blackberry.com>
2015-05-15 12:58:50 -0400

    One at a time

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 
 // ========== jQuery on click declarations
 var boxes = 0;
+var title, textData;
 $(function(){
-  $(document).on('click','.grab', function(){pasteSelection(this.id)});
+  $(document).on('click','.grab', function(){setText(this.id)});
 });
 
 $(function(){
-  $('#addnew').click(function(){addnew();});
+  $('#addnew').click(function(){addnew(boxes);});
 });
 
 // ========= Functions
 
 // Get selected text and paste
-function pasteSelection(bid) {
-  putTitle(bid);
+function setText(bid) {
+  setTitle(bid);
   var text = document.getElementById("data".concat(bid.toString()));
   chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
-  function(tab) {
+  function(tab){
     chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, 
     function(response){
       if (response.data == null){
@@ -28,39 +29,45 @@ function pasteSelection(bid) {
       }
     });
   });
-  addnew();
+  textData = text;
+  addnew(bid);
 }
 
-function putTitle(bid){
+function setTitle(bid){
   var p1 = document.getElementById("title".concat(bid.toString()));
-  console.log("title".concat(bid.toString()));
   chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     p1.innerHTML = "<b>" + tabs[0].title + "</b>";
+    title = tabs[0].title;
+    console.log(title);
   });
 }
 
 // Add new paste box
-function addnew(){
-  boxes+=1;
-  var button = document.createElement("BUTTON");
-  button.setAttribute("class","list-group-item grab")
-  button.id = boxes.toString();
-  var p = document.createElement("p");
-  p.setAttribute("class", "list-group-item-heading pull-left data");
-  p.innerHTML = "<b>Click To Paste</b>"
-  p.id = "title".concat(boxes.toString());
-  var p2 = document.createElement("p");
-  p2.setAttribute("class", "list-group-item-text pull-left data");
-  p2.id = "data".concat(boxes.toString());
-  button.appendChild(p);
-  button.appendChild(p2);
-  document.getElementById("snippits").appendChild(button);
+function addnew(bid){
+  if (bid < boxes){return;}
+  else{
+    boxes+=1;
+    var button = document.createElement("BUTTON");
+    button.setAttribute("class","list-group-item grab")
+    button.id = boxes.toString();
+    var p = document.createElement("p");
+    p.setAttribute("class", "list-group-item-heading pull-left data");
+    p.innerHTML = "<b>Click To Paste</b>"
+    p.id = "title".concat(boxes.toString());
+    var p2 = document.createElement("p");
+    p2.setAttribute("class", "list-group-item-text pull-left data");
+    p2.id = "data".concat(boxes.toString());
+    button.appendChild(p);
+    button.appendChild(p2);
+    document.getElementById("snippits").appendChild(button);
+    console.log([title, textData][0]);
+  }
 }
 
-function store(bid, value){
+function store(bid){
+  var snip = JSON.stringify(getSnippit(bid));
   bid = bid.toString();
-  value = JSON.stringify(value);
-  chrome.storage.local.set({bid: value}, function (result) {
+  chrome.storage.sync.set({bid: snip}, function (result) {
     return;
   }); 
 }
