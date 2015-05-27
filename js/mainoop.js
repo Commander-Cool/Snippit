9fe26cea17573cf499e54407509dc25b227d381d
Jay Kumar <jakumar@blackberry.com>
2015-05-27 16:19:52 -0400

    More OOP

--- js/mainoop.js
+++ js/mainoop.js
@@ -1,65 +1,71 @@
 var boxes = 0;
+var snippits = {};
 
 $(function(){
   $(document).on('click','.grab', function(){setText(this.id)});
 });
 
 $(function(){
-  $('#addnew').click(function(){addnew(boxes);});
+  $('#addnew').click(function(){addnew();});
 });
 
 function Snippit(id){
-	var snippit = Object.create(Snippit.prototype);
-	snippit.id = id;
+	document.getElementById("snippits").appendChild(button);
+	var snippit   = Object.create(Snippit.prototype);
 	snippit.title = "<b>Click To Paste</b>"
-	snippit.data = "";
+	snippit.data  = "";
+	snippit.id 	  = id;
 	return snippit;
 };
 
 Snippit.prototype.setTitle = function (){
-	var p1 = document.getElementById("title".concat(Snippit.id));
+	var p1 = document.getElementById("title".concat(this.id));
   	chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-  		Snippit.title = tabs[0].title;
-    	p1.innerHTML = "<b>" + Snippit.title + "</b>";
+  		this.title = tabs[0].title;
+    	p1.innerHTML = "<b>" + this.title + "</b>";
   	});
+  	setText();
 };
 
 Snippit.prototype.setText = function (){
-  	var text = document.getElementById("data".concat(Snippit.id));
+  	var text = document.getElementById("data".concat(this.id));
   	chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
   	function(tab){
     	chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, 
     	function(response){
       		if (response.data == null){
         		chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-        		Snippit.data = tabs[0].url;
+        		this.data = tabs[0].url;
         	});
       		}else{
-      			Snippit.data = response.data;
+      			this.data = response.data;
       		}
     	});
   	});
-  	text.innerHTML = Snippit.data;
+  	text.innerHTML = this.data;
 };
 
-function addNew(Snippit){
+function createButton(snippit){
+	var button = document.createElement("BUTTON");
+    button.setAttribute("class","list-group-item grab")
+    button.id = snippit.id.toString();
+    var p = document.createElement("p");
+    p.setAttribute("class", "list-group-item-heading pull-left data");
+    p.innerHTML = snippit.title;
+    p.id = "title".concat(snippit.id.toString());
+    var p2 = document.createElement("p");
+    p2.setAttribute("class", "list-group-item-text pull-left data");
+    p2.id = "data".concat(snippit.id.toString());
+    button.appendChild(p);
+    button.appendChild(p2);
+};
+
+function addNew(id){
 	if (id < boxes){
 		return;
 	}else{
 		boxes+=1;
-	    var button = document.createElement("BUTTON");
-	    button.setAttribute("class","list-group-item grab")
-	    button.id = boxes.toString();
-	    var p = document.createElement("p");
-	    p.setAttribute("class", "list-group-item-heading pull-left data");
-	    p.innerHTML = "<b>Click To Paste</b>"
-	    p.id = "title".concat(boxes.toString());
-	    var p2 = document.createElement("p");
-	    p2.setAttribute("class", "list-group-item-text pull-left data");
-	    p2.id = "data".concat(boxes.toString());
-	    button.appendChild(p);
-	    button.appendChild(p2);
-	    document.getElementById("snippits").appendChild(button);
+		snippits[boxes] = new Snippit(boxes);
 	};
 };
 
